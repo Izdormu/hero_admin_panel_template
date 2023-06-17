b717beab2b0ca802a15ad54f43859060ce37aeb5
Vladyslav <billymosh@gmail.com>
2023-06-17 13:47:07 +0300

    added delete func for hero onclock

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,8 +1,8 @@
-import {useHttp} from '../../hooks/http.hook';
+import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -12,9 +12,9 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const { heroes, heroesLoadingStatus } = useSelector(state => state);
     const dispatch = useDispatch();
-    const {request} = useHttp();
+    const { request } = useHttp();
 
     useEffect(() => {
         dispatch(heroesFetching());
@@ -25,8 +25,19 @@ const HeroesList = () => {
         // eslint-disable-next-line
     }, []);
 
+    const onDelete = (id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(
+                data => console.log(data, "hero deleted"))
+            .then(
+                dispatch(heroDeleted(id))
+            )
+            .catch(err => console.log(err))
+    }
+
+
     if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
+        return <Spinner />;
     } else if (heroesLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
     }
@@ -36,8 +47,8 @@ const HeroesList = () => {
             return <h5 className="text-center mt-5">Героев пока нет</h5>
         }
 
-        return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+        return arr.map(({ id, ...props }) => {
+            return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)} />
         })
     }
 
